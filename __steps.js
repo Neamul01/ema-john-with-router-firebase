284ed87b1811f02b4af6084446e483ced09bcb7b
Neamul01 <neamulhaque2002@gmail.com>
2022-04-10 16:10:52 +0600

    sign up function some done

--- __steps.js
+++ __steps.js
@@ -5,4 +5,6 @@
  * 4. Firebase setting > authentication > enable email and password auth
  * 5. Create log in sign up component and route
  * 6. form field handler and form submit handler
+ * 7. npm i --save react-firebase-hooks
+ * 8. useCreateUserWithEmailAndPassword from react firebase hooks
  */
--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import './Signup.css'
+import auth from '../../firebase.init'
 
 const Signup = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [confirmPassword, setConfirmPassword] = useState('')
-    const [error, setError] = useState('')
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate()
+
+    const [createUserWithEmailAndPassword, user] = useCreateUserWithEmailAndPassword(auth)
 
     const handleEmailBlur = event => {
         setEmail(event.target.value)
@@ -20,12 +25,21 @@ const Signup = () => {
         setConfirmPassword(event.target.value)
     }
 
+    if (user) {
+        navigate('/shop')
+    }
+
     const handleCreateUser = event => {
         event.preventDefault()
         if (password !== confirmPassword) {
             setError('Your Password did not match')
             return;
         }
+        if (password < 6) {
+            setError('Password must be 6 charecter or longer')
+            return
+        }
+        createUserWithEmailAndPassword(email, password)
     }
 
 
